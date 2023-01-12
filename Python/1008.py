e3a1ca4244187fb726c7ad7a196d6a1da330aabd
Daniela Lima <96999239+dani7fl7@users.noreply.github.com>
2023-01-12 13:50:55 -0300

    Create 1008.py

---
+++ Python/1008.py
@@ -0,0 +1,8 @@
+numfunc = int(input())
+numhoras = int(input())
+valorporhora = float(input())
+
+salario = (numhoras * valorporhora)
+
+print("NUMBER = %.f" % numfunc)
+print("SALARY = U$ %.2f" % salario)
