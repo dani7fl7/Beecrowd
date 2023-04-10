40e7205b024821828242b0848169b53f1de7e465
Daniela Lima <96999239+dani7fl7@users.noreply.github.com>
2023-04-10 09:43:58 -0300

    Create 1010.py

---
+++ Beginner/Python/1010.py
@@ -0,0 +1,8 @@
+linha1 = input().split(" ")
+linha2 = input().split(" ")
+
+codigo1, numero1, valor1 = linha1 
+codigo2, numero2, valor2 = linha2 
+
+total = (int(numero1)*float(valor1)) + (int(numero2)*float(valor2))
+print("VALOR A PAGAR: R$ %0.2f" %total)
